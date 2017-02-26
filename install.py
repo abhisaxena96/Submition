0ef000f4a057ebd18fee60c8accca5339a790ce4
abhisaxena96 <abhi.saxena96@gmail.com>
2017-02-26 13:15:38 +0530

    Add files via upload

---
+++ install.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+import os
+os.system("sudo apt-get update ")
+os.system("sudo apt-get install nginx -y")
+os.system("sudo apt-get install php-fpm php-mysql -y")
+os.system("sudo apt-get install mysql-server  -y")
+
