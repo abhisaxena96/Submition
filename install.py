7f2c47969d6e6d366aa63cf7c0bb908bed6b2b7e
abhisaxena96 <abhi.saxena96@gmail.com>
2017-02-26 14:09:49 +0530

    Delete install.py

--- install.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python
-
-import os
-os.system("sudo apt-get update ")
-os.system("sudo apt-get install nginx -y")
-os.system("sudo apt-get install php-fpm php-mysql -y")
-os.system("sudo apt-get install mysql-server  -y")
-
